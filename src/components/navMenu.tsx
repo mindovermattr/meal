e59c65fe8e1f431c8a7e9fb5fed699025467cc8c
mindovermattr <dduim@bk.ru>
2022-11-19 07:51:07 +0300

    adaptive

--- src/components/navMenu.tsx
+++ src/components/navMenu.tsx
@@ -2,19 +2,19 @@ import { FunctionComponent } from "react";
 import { Link } from "react-router-dom";
 import CustomInput from "../UI/CustomInput/CustomInput";
 
-interface HeaderProps {
-    
-}
+
  
-const NavMenu: FunctionComponent<HeaderProps> = () => {
+const NavMenu = () => {
     return ( 
     <header className="bg-main-color">
         <div className="header__wrapper container">
-            <nav className="header__content">
-                <Link to="/home"  className="nav__item">Home</Link>
-                <Link to="/recipes" className="nav__item">Recipes</Link>
-                <Link to="/Profile" className="nav__item">Account</Link>
-                <Link to="/Search" className="nav__item">Search</Link>
+            <nav>
+                <ul className="header__content">
+                    <li><Link to="/home"  className="nav__item">Home</Link></li>
+                    <li><Link to="/recipes" className="nav__item">Recipes</Link></li>
+                    <li><Link to="/Profile" className="nav__item">Account</Link></li>
+                    <li><Link to="/Search" className="nav__item">Search</Link></li>
+                </ul>
             </nav>
         </div>
         
